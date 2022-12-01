50fe2dc3fb7178f3c969070dcedbb2c1f8461693
Michael Schuetze <mjschuetze102@gmail.com>
2022-12-01 01:33:27 -0500

    Completed Day 1

---
+++ Day1/Part1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Part1
+    {
+        static void Temp()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\User\Downloads\advent.txt");
+
+            int currCalories = 0;
+            int mostCalories = 0;
+            foreach (string line in lines)
+            {
+                if (line == "")
+                {
+                    if (currCalories > mostCalories) mostCalories = currCalories;
+
+                    currCalories = 0;
+                    continue;
+                }
+
+                currCalories += Int32.Parse(line);
+            }
+
+            if (currCalories > mostCalories) mostCalories = currCalories;
+
+            Console.WriteLine($"Calories: {mostCalories}");
+        }
+    }
+}
---
+++ Day1/Part2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Part2
+    {
+        static void Main()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\User\Downloads\advent.txt");
+
+            int currCalories = 0;
+            List<int> mostCalories = new List<int>() { 0, 0, 0 };
+
+            foreach (string line in lines)
+            {
+                if (line == "")
+                {
+                    if (currCalories > mostCalories[0])
+                    {
+                        mostCalories.RemoveAt(0);
+
+                        for (int index = 0; index < mostCalories.Count; index++)
+                        {
+                            if (currCalories < mostCalories[index])
+                            {
+                                mostCalories.Insert(index, currCalories);
+                                break;
+                            }
+                        }
+
+                        if (mostCalories.Count != 3) mostCalories.Add(currCalories);
+                    }
+
+                    currCalories = 0;
+                    continue;
+                }
+
+                currCalories += Int32.Parse(line);
+            }
+
+            if (currCalories > mostCalories[0])
+            {
+                mostCalories.RemoveAt(0);
+                mostCalories.Add(currCalories);
+            }
+
+            Console.WriteLine($"Calories: {mostCalories.Sum()}");
+        }
+    }
+}
