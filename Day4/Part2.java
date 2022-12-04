bab794e2d2c6b2208da3a0473c7d76765b2a2ad4
Michael Schuetze <mjschuetze102@gmail.com>
2022-12-04 00:42:21 -0500

    Completed Day 4

---
+++ Day4/Part2.java
@@ -0,0 +1,37 @@
+import javafx.util.Pair;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Part2 {
+
+    public static void main(String[] args) throws IOException {
+        String file = "C:\\Users\\User\\Downloads\\advent.txt";
+
+        int count = 0;
+        List<Pair<Integer, Integer>> elves = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            for(String line; (line = reader.readLine()) != null; ) {
+                String[] assignments = line.split(",");
+
+                for(String section: assignments) {
+                    String[] parts = section.split("-");
+                    Pair<Integer, Integer> range = new Pair<>(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+
+                    elves.add(range);
+                }
+
+                if (
+                    (elves.get(0).getKey() <= elves.get(1).getKey() && elves.get(0).getValue() >= elves.get(1).getKey()) ||
+                    (elves.get(0).getKey() >= elves.get(1).getKey() && elves.get(0).getKey() <= elves.get(1).getValue())
+                )   count++;
+
+                elves.clear();
+            }
+        }
+
+        System.out.println(count);
+    }
+}
